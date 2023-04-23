80de8be2062c3a823c41441a742816112a559b3c
besser <besser9009@mail.ru>
2023-04-23 13:53:45 +0300

    add Num2

---
+++ Num2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+void NewMethod(string whith = "1", string on = "3", string some = "2", int count = 2)
+{
+    if (count > 1) NewMethod(whith, some, on, count - 1);
+    System.Console.WriteLine(count);
+    System.Console.WriteLine($"{whith} >> {on}");
+    if (count > 1) NewMethod(some, on, whith, count - 1);
+}
+NewMethod();
